7ef9afc0bc6fbc66ac336da5aae9f871d7b319ae
can23384 <can23384@uvg.edu.gt>
2024-03-13 20:43:16 -0600

    2

--- proyecto.lisp
+++ proyecto.lisp
@@ -1,10 +1,19 @@
-;; Definición de la función factorial usando DEFUN
+;;; Calcula el factorial de un número de forma recursiva.
+;;;
+;;; @param n El número entero del cual se calculará el factorial.
+;;; @return El factorial de n.
 (defun factorial (n)
+  "Calcula el factorial de un número de forma recursiva."
   (cond ((= n 0) 1)               ; Caso base: factorial de 0 es 1
         (t (* n (factorial (- n 1))))))  ; Caso recursivo: n * factorial(n-1)
 
-;; Uso de predicados para validar el argumento de entrada
+;;; Envuelve la función factorial para realizar validaciones de entrada.
+;;;
+;;; @param n El número entero del cual se calculará el factorial.
+;;; @return El factorial de n, si n es un número entero no negativo.
+;;; @error Si n es una lista, no es un número entero, o es negativo.
 (defun factorial-wrapper (n)
+  "Envuelve la función factorial para realizar validaciones de entrada."
   (cond ((listp n)               ; Comprobación si n es una lista
          (error "El argumento no puede ser una lista"))
         ((not (integerp n))      ; Comprobación si n no es un entero
@@ -13,8 +22,7 @@
          (error "El argumento debe ser un entero no negativo"))
         (t (factorial n))))      ; Llamar a la función factorial si n es válido
 
-;; Ejemplo de uso
-(print "Ingrese el numero: ")
+;;; Solicita al usuario un número y muestra su factorial.
+(format t "Ingrese el numero: ")
 (setq n (read))
-
-(format t "El factorial de ~d es: ~d~%" 'n (factorial-wrapper n))
+(format t "El factorial de ~d es: ~d~%" n (factorial-wrapper n))
